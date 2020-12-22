325f66a11d9cb7ab620190a5a7a1c171dd128788
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2020-12-22 23:08:24 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: AssetImage('images/profile.jpeg'),
+                ),
+                Text(
+                  'Korbinian Schleifer',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Playfair-Display',
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.grey[300],
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          selectedFontSize: 15.0,
+          selectedItemColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group),
+              label: 'Friends',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
